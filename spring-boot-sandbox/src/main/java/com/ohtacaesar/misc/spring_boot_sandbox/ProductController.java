ac620b0f05e9b11237bb6bdef495f7da9a98da34
ohtacaesar <ohtacaesar@gmail.com>
2019-01-19 14:36:58 +0900

    [spring-boot-sandbox] Formまわりいろいろ

--- spring-boot-sandbox/src/main/java/com/ohtacaesar/misc/spring_boot_sandbox/ProductController.java
+++ spring-boot-sandbox/src/main/java/com/ohtacaesar/misc/spring_boot_sandbox/ProductController.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,6 +24,9 @@ public class ProductController {
   @GetMapping
   public String index(Model model) {
     model.addAttribute("productList", productRepository.findAll());
+    if (!model.containsAttribute("product")) {
+      model.addAttribute("product", new Product());
+    }
 
     return "product/index";
   }
@@ -38,8 +45,39 @@ public class ProductController {
   }
 
   @GetMapping(value = "/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
   public Product showJson(@PathVariable int productId) {
     return productRepository.findOne(productId);
+  }
+
+  @PostMapping
+  public String create(
+      Model model,
+      // @Validatedつけるとエラーページに飛ばない
+      @Validated Product product,
+      BindingResult bindingResult
+  ) {
+
+    if (bindingResult.hasErrors()) {
+      return index(model);
+    }
+
+    productRepository.save(product);
+
+    return "redirect:/products";
+  }
+
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
+  public Object createJson(
+      @RequestBody @Validated Product product,
+      BindingResult bindingResult
+  ) {
+    if (bindingResult.hasErrors()) {
+      return bindingResult.getAllErrors();
+    }
+
+    return productRepository.save(product);
   }
 
 }
