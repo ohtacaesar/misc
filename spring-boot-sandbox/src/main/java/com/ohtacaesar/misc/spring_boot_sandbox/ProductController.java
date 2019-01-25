046ff7ba6d2e9996ed01559f88950f7f904c7287
ohtacaesar <ohtacaesar@gmail.com>
2019-01-25 22:10:05 +0900

    [spring-boot-sandbox] プロダクトにタグを追加・削除する

--- spring-boot-sandbox/src/main/java/com/ohtacaesar/misc/spring_boot_sandbox/ProductController.java
+++ spring-boot-sandbox/src/main/java/com/ohtacaesar/misc/spring_boot_sandbox/ProductController.java
@@ -1,7 +1,10 @@
 package com.ohtacaesar.misc.spring_boot_sandbox;
 
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -9,20 +12,26 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 
   @Autowired
   private ProductRepository productRepository;
+
+  @Autowired
+  private TagRepository tagRepository;
 
   @GetMapping
   public String index(Model model) {
@@ -43,6 +52,7 @@ public class ProductController {
   @GetMapping("/{productId}")
   public String show(Model model, @PathVariable int productId) {
     model.addAttribute("product", productRepository.findOne(productId));
+    model.addAttribute("tagList", tagRepository.findAll());
 
     return "product/show";
   }
@@ -79,6 +89,41 @@ public class ProductController {
 
     return "redirect:/products";
   }
+
+  @Transactional
+  @PostMapping("/{productId}/tags")
+  public String addTag(@PathVariable int productId, @RequestParam int tagId) {
+    log.info("addTag(productId={}, tagId={})", productId, tagId);
+    Product product = productRepository.findOne(productId);
+    if (product == null) {
+      throw new NotFoundException();
+    }
+    Tag tag = tagRepository.findOne(tagId);
+    if (tag == null) {
+      throw new NotFoundException();
+    }
+
+    product.getTagList().add(tag);
+
+    return String.format("redirect:/products/%d", productId);
+  }
+
+  @Transactional
+  @DeleteMapping("/{productId}/tags/{tagId}")
+  public String removeTag(@PathVariable int productId, @PathVariable int tagId) {
+    // Transactionalに任せると一度全関連DELETEしてから、再度登録しなおすっぽい
+    log.info("removeTag(productId={}, tagId={})", productId, tagId);
+    Product product = productRepository.findOne(productId);
+    if (product == null) {
+      throw new NotFoundException();
+    }
+
+    Optional<Tag> tag = product.getTagList().stream().filter(o -> o.getId() == tagId).findFirst();
+    tag.ifPresent(tag1 -> product.getTagList().remove(tag1));
+
+    return String.format("redirect:/products/%d", productId);
+  }
+
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
