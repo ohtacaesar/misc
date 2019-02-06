f834ef7a99fcfcf9adcf34f117defd740e586286
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2019-02-06 23:32:31 +0900

    [spring-boot-sandbox] 実行されたSQLをリストに格納する

---
+++ spring-boot-sandbox/src/main/java/com/ohtacaesar/misc/spring_boot_sandbox/HibernateInterceptor.java
@@ -0,0 +1,35 @@
+package com.ohtacaesar.misc.spring_boot_sandbox;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.EmptyInterceptor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HibernateInterceptor extends EmptyInterceptor {
+
+  private static final ThreadLocal<List<String>> sqlList = new ThreadLocal<>();
+
+  public void init() {
+    System.out.println(getClass() + "#init: " + Thread.currentThread().getId());
+    sqlList.set(new ArrayList<>());
+  }
+
+  public List<String> getSqlList() {
+    return sqlList.get();
+  }
+
+  @Override
+  public String onPrepareStatement(String sql) {
+    System.out.println(getClass() + "#onPrepareStatement: " + Thread.currentThread().getId());
+    System.out.println(sql);
+    List<String> list = this.getSqlList();
+    System.out.println(list);
+
+    if (list != null) {
+      list.add(sql);
+    }
+    return sql;
+  }
+
+}
