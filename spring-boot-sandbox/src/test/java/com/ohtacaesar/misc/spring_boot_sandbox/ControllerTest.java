8a3b077edff3967a0199d179ef5bed9a9b07f15f
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2019-02-19 00:14:33 +0900

    [spring-boot-sandbox] FunctionalTest

---
+++ spring-boot-sandbox/src/test/java/com/ohtacaesar/misc/spring_boot_sandbox/ControllerTest.java
@@ -0,0 +1,35 @@
+package com.ohtacaesar.misc.spring_boot_sandbox;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ControllerTest {
+
+  @Autowired
+  private TestRestTemplate testRestTemplate;
+
+  @Test
+  public void test() {
+    ResponseEntity actual = testRestTemplate.exchange(
+        "/companies/1",
+        HttpMethod.GET,
+        new HttpEntity<>(null, null),
+        String.class
+    );
+
+    Assert.assertEquals(HttpStatus.OK, actual.getStatusCode());
+  }
+
+}
